ef9f9960422af4f93f259d9118565eb9dc7fd7fe
MartijnK96 <m.c.l.kamphuis@student.rug.nl>
2020-12-03 14:14:58 +0100

    Made some change to program for generating XY points.

--- src/create_coordinate_points.py
+++ src/create_coordinate_points.py
@@ -8,8 +8,7 @@ try:
 except:
     machines, sections, charging_stations = 4, 8, 2
 
-type(machines)
-
+space_left = 10000
 wall_to_IS = 5090
 width_IS = 5150
 distance_IS = 6610
@@ -18,6 +17,21 @@ distance_SEC = 7532/(sections+1)
 separation_SEC = 300
 
 with open("coordinate_points.csv", "w+") as f:
-    for i in range(1,machines+1):
-        for i in range(1,sections+1):
-            
+    writer = csv.writer(f)
+
+    for i in range(machines):
+        label = "p" + str(i+1)
+        x_coor_mm = space_left-2500
+        x_coor_m = x_coor_mm/1000
+        y_coor_mm = wall_to_IS + i*(width_IS+distance_IS)
+        y_coor_m = y_coor_mm/1000
+        writer.writerow([label, x_coor_m, y_coor_m])
+
+    for i in range(machines):
+        for j in range(sections):
+            label = "m" + str(i+1) + str(j+1)
+            x_coor_mm = space_left + (distance_SEC*(j+1))
+            x_coor_m = x_coor_mm/1000
+            y_coor_mm = wall_to_IS + i*(width_IS+distance_IS)
+            y_coor_m = y_coor_mm/1000
+            writer.writerow([label, x_coor_m, y_coor_m])
